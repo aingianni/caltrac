67b165fbda5b182b820cdf0d3876d784563d1e7e
Anthony Ingianni <ingianni.anthony@gmail.com>
2023-02-26 09:06:33 -0500

    updated diet.js

---
+++ routes/api/diet.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { dataController, apiController } = require('../../controllers/api/diet');
+
+router.get('/', dataController.index, apiController.index);
+router.delete('/:id', dataController.destroy, apiController.show);
+router.put('/:id', dataController.update, apiController.show);
+router.post('/', dataController.create, apiController.show);
+router.get('/:id', dataController.show, apiController.show);
+
+module.exports = router;
